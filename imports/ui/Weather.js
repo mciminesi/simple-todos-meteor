b5a24258386871ad15e6c1d03a52d544ad81e0be
Theo <17056323+eXrump@users.noreply.github.com>
2018-07-18 18:46:23 -0400

    Future replaced by promise.

--- imports/ui/Weather.js
+++ imports/ui/Weather.js
@@ -11,7 +11,6 @@ export default class Weather extends Component {
 
     componentDidMount() {
         Meteor.call('getWeather', (error, result) => {
-            //console.log(result);
             this.setState({ weather_data: result });
         });
     }
@@ -19,12 +18,12 @@ export default class Weather extends Component {
     render() {
         const weather_data = this.state.weather_data;
         if (!weather_data) return <div><br></br>Loading...</div>
-        console.log(this.state.weather_data);
+
         return(
             <div>
                 <header className="headerReverse">
                     <h1>
-                        {weather_data.name + " "}
+                        {weather_data.name + ":  "}
                         {weather_data.weather[0].description.toUpperCase()}
                         <img src={"http://openweathermap.org/img/w/" + weather_data.weather[0].icon + ".png"}/>
                         {Math.round(weather_data.main.temp) + "°"}
--- server/main.js
+++ server/main.js
@@ -1,30 +1,35 @@
 import { Meteor } from 'meteor/meteor';
 import '../imports/api/tasks.js';
+import { resolve } from 'url';
+import { rejects } from 'assert';
 var request = require('request');
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
-    Future = Npm.require('fibers/future');
   });
 
   Meteor.methods({
     'getWeather' () {
       // Construct the API URL
-      var future = new Future();
-      var apiKey = Meteor.settings.apiKey;
       var cityId = Meteor.settings.cityId;
+      var apiKey = Meteor.settings.apiKey;
       var apiUrl = "http://api.openweathermap.org/data/2.5/weather?id=" + cityId + "&units=imperial&APPID=" + apiKey;
-      // query the API
-      request(apiUrl, { json: true },  (error, response, body) => {
-        if (error) { 
-          return console.log(error); 
-        }
+
+      const promise = new Promise((resolve, reject) => {
+        request(apiUrl, { json: true },  function(error, response) {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(response.body);
+            }
+          });
+        })
+        .catch((e) => {
+          throw new Meteor.Error('500', e.message);
+        });
       
-        future["return"](response.body);
-      });
-      
-      return future.wait();
+      return promise.await();
     },
   });
 }
